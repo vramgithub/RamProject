48206ab0ca8a3a5b4cf3b32dd243370d0e64ab1f
vramgithub <vramjira93@gmail.com>
2023-08-01 02:25:59 +0530

    last push by B

--- src/org/git/Demo.java
+++ src/org/git/Demo.java
@@ -3,11 +3,9 @@ package org.git;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Ramprasad");
-<<<<<<< HEAD
-		System.out.println("Conflict line");
-=======
 		System.out.println("Akash");
->>>>>>> 5bd7818041a628416af7e84b75c5e19f39caa250
+		System.out.println("Conflict line");
+
 	}
 
 }
